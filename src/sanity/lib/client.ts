5b26c6dee3fcac1d226b1706613c626ecea428f5
AbdulAhadAfridi <hafizabdulahadkhanafidi@gmail.com>
2024-12-29 20:22:16 +0500

    first-change

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,6 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  token: process.env.SANITYskYMyvyLLkN6DdYb7EN66LWxg66fmuZYNY9ULXBfF9PZQTS9UjrkmIB4lC1I11gu5Rnv0iF78UufwnT945PIRhf64g89TOsL5BGCO9exuPJAEi3DQXhARYBAMSV67pou10eiglk8KsMP1rgIx9NAtWLpZj38gKlqQs8OFk2M0ONmBUUz7PdO,  // Use the API token for authenticated requests
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
